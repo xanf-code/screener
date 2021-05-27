ad72c05eeb5a531415b895943ae4181ba0951964
xanf-code <darshanaswath@gmail.com>
2021-05-27 22:38:46 +0530

    new

--- routes/screener.js
+++ routes/screener.js
@@ -1,66 +1,40 @@
 const express = require("express");
-const request = require("request-promise");
 const cheerio = require("cheerio");
-const Screener = require("../models/screener_model");
-
 const router = express.Router();
+const Screener = require('../models/screener_model');
+const axios = require('axios')
 
-//ScrapeData
 router.get("/screener/:id", async (req, res) => {
     try {
         const query = req.params.id;
-        await request(
-            `https://www.insiderscreener.com/en/explore?page=${query}&nb_shares=1&sort_by=transaction_date&sort_order=descending&regulator=US&regulator=FR&regulator=DE&regulator=CH&regulator=BE&regulator=ES&regulator=NL&regulator=SE&regulator=IT&regulator=GR&regulator=IN&transaction_type=BUY&transaction_type=SELL&transaction_type=PLANNED_PURCHASE&transaction_type=PLANNED_SALE&position_type=1&position_type=2&position_type=3&position_type=4&position_type=5&position_type=6&position_type=7&position_type=8&position_type=9`,
-            {
-                headers: {
-                    "accept": "*/*",
-                    "accept-language": "en-US,en-IN;q=0.9,en-UM;q=0.8,en;q=0.7",
-                    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-origin",
-                    "x-requested-with": "XMLHttpRequest",
-                },
+        const response = await axios.get(
+            `https://www.insiderscreener.com/en/explore?page=${query}&nb_shares=1&sort_by=transaction_date&sort_order=descending&regulator=US&regulator=FR&regulator=DE&regulator=CH&regulator=BE&regulator=ES&regulator=NL&regulator=SE&regulator=IT&regulator=GR&regulator=IN&transaction_type=BUY&transaction_type=SELL&transaction_type=PLANNED_PURCHASE&transaction_type=PLANNED_SALE&position_type=1&position_type=2&position_type=3&position_type=4&position_type=5&position_type=6&position_type=7&position_type=8&position_type=9`, {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "en-US,en-IN;q=0.9,en-UM;q=0.8,en;q=0.7",
+                "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-requested-with": "XMLHttpRequest",
             },
-            (error, response, html) => {
-                scrapefunction(error, response, html);
-            }
-        ).then(() => {
-            res.send({
-                type: "Data Scraped",
-                status: 200,
-            });
+            "referrer": `https://www.insiderscreener.com/en/explore?page=${query}&nb_shares=1&sort_by=transaction_date&sort_order=descending&regulator=US&regulator=FR&regulator=DE&regulator=CH&regulator=BE&regulator=ES&regulator=NL&regulator=SE&regulator=IT&regulator=GR&regulator=IN&transaction_type=BUY&transaction_type=SELL&transaction_type=PLANNED_PURCHASE&transaction_type=PLANNED_SALE&position_type=1&position_type=2&position_type=3&position_type=4&position_type=5&position_type=6&position_type=7&position_type=8&position_type=9`,
+            "referrerPolicy": "same-origin",
+            "method": "GET",
+            "mode": "cors"
         });
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-});
-
-function scrapefunction(error, response, html) {
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        $(
-            "#transactions > div > div > div.table-responsive-md > table > tbody > tr"
-        ).each((index, el) => {
+        const $ = cheerio.load(response.data);
+        $('#transactions > div > div > div.table-responsive-md > table > tbody > tr').each((index, el) => {
             const notificationDate = $(el).find("td:nth-child(2)").text().trim();
             const transactionDate = $(el).find("td:nth-child(3)").text().trim();
             const companyName = $(el)
                 .find("td:nth-child(4) > div > a:nth-child(1)")
                 .text()
                 .trim();
-            const ticker = $(el)
-                .find("td:nth-child(4) > div > span")
-                .text()
-                .replace("(", " ")
-                .replace(")", " ")
-                .trim();
+            const ticker = $(el).find("td:nth-child(4) > div > span").text().replace("(", " ").replace(")", " ").trim();
             const companyType = $(el).find("td:nth-child(4) > small").text().trim();
-            const insiderName = $(el)
-                .find("td:nth-child(6) > p:nth-child(1)")
-                .text()
-                .trim();
+            const insiderName = $(el).find("td:nth-child(6) > p:nth-child(1)").text().trim();
             const insiderTitle = $(el).find("td:nth-child(6) > span").text().trim();
             const tradeType = $(el)
                 .find("td:nth-child(5) > span > span.d-none.d-sm-block")
@@ -68,16 +42,12 @@ function scrapefunction(error, response, html) {
                 .trim();
             const tradePrice = $(el).find("td:nth-child(9)").text().trim();
             const quantityshares = $(el).find("td:nth-child(8)").text().trim();
-            const percentage = $(el)
-                .find("td:nth-child(8) > span > i > b")
-                .text()
-                .trim();
+            const percentage = $(el).find("td:nth-child(8) > span > i > b").text().trim();
             const value = $(el)
                 .find(
                     "td.font-weight-bold.align-middle.text-right.d-none.d-sm-table-cell > span"
                 )
-                .text()
-                .replace(/[\n\t\r]/g, " ")
+                .text().replace(/[\n\t\r]/g, " ")
                 .trim();
             const countryCode = $(el).find("td:nth-child(1) > img").attr("alt");
             const countryImage = $(el).find("td:nth-child(1) > img").attr("src");
@@ -102,15 +72,29 @@ function scrapefunction(error, response, html) {
                 url: {
                     CompanyLink: companyLink,
                     CountryImage: countryImage,
-                },
+                }
             });
             screener.save();
         });
+        res.send({
+            // page: parseInt(query),
+            // totalPage: 100,
+            // nextPage: parseInt(query) + 1,
+            // lastPage: parseInt(query) - 1,
+            // perPage: state.length,
+            status: 200,
+            // results: scrapedData,
+        });
+    } catch (e) {
+        res.send({
+            status: 400,
+            error: e.message,
+        });
     }
-}
+});
 
 //GET All insider
-router.get("/data", async (req, res) => {
+router.get('/data', async (req, res) => {
     try {
         const limit = req.query.limit ? parseInt(req.query.limit) : 75;
         const page = req.query.page ? parseInt(req.query.page) : 1;
@@ -127,16 +111,31 @@ router.get("/data", async (req, res) => {
             previousPage: page - 1,
             perPage: limit,
             totalPages: parseInt((count / limit).toFixed()),
-            isNextPageExist: page + 1 <= count / limit ? true : false,
-            isLastPageExist: page - 1 == 0 ? false : true,
+            isNextPageExist: (page + 1) <= (count / limit) ? true : false,
+            isLastPageExist: (page - 1) == 0 ? false : true,
             result: result,
         });
-    } catch (e) {
+    }
+    catch (e) {
         res.send({
             status: 400,
             error: e.message,
         });
     }
-});
+})
+
+//InsiderName Individual
+router.get('/screener/:insiderName', async (req, res) => {
+    try {
+        const result = await Screener.find({ InsiderName: req.params.insiderName }, "-__v");
+        res.status(200).json({ serverTime: Date.now(), total: result.length, result });
+    }
+    catch (err) {
+        res.send({
+            status: 400,
+            error: e.message,
+        });
+    }
+})
 
 module.exports = router;
