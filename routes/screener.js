63d9c1de9b10e94705c0ae9774e26229f49c9078
xanf-code <darshanaswath@gmail.com>
2021-05-28 23:55:16 +0530

    deploy

--- routes/screener.js
+++ routes/screener.js
@@ -14,6 +14,9 @@ async function scrapeInsider(param) {
         ]
     });
     const page = await browser.newPage();
+
+    page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36');
+
     await page.goto(`https://www.insiderscreener.com/en/explore?page=${param}&nb_shares=1&sort_by=transaction_date&sort_order=descending&regulator=US&regulator=FR&regulator=DE&regulator=CH&regulator=BE&regulator=ES&regulator=NL&regulator=SE&regulator=IT&regulator=GR&regulator=IN&transaction_type=BUY&transaction_type=SELL&transaction_type=PLANNED_PURCHASE&transaction_type=PLANNED_SALE&position_type=1&position_type=2&position_type=3&position_type=4&position_type=5&position_type=6&position_type=7&position_type=8&position_type=9`);
     const html = await page.content();
     const $ = cheerio.load(html);
